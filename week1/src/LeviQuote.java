01bd560ff9cfc6ed164ac030b32a19a28df9faa0
levi <levi.devhaus@gmail.com>
2025-07-28 15:55:32 +0930

    week1 - 2nd supplementary video

---
+++ week1/src/LeviQuote.java
@@ -0,0 +1,9 @@
+public class LeviQuote
+{
+    //    print quotes from Levi
+    //----------------------------------------------------------------
+    public static void main(String[] args) {
+        System.out.println("Consistency is the answer");
+        System.out.println("-- Levi Vu");
+    }
+}
